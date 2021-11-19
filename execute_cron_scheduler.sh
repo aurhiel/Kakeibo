0efcce484d63a3061ca85dfe89455b68da1729e1
Aurélien Litti <litti.aurelien@gmail.com>
2021-11-19 14:41:01 +0100

    add: prod shell execute cron command edit(automaton): add overflow in recurrent transactions list

--- execute_cron_scheduler.sh
+++ execute_cron_scheduler.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-bin/console scheduler:execute
+# PROD
+# /usr/local/php7.3/bin/php /home/everyday/la/dev/kakeibo/bin/console scheduler:execute
+/usr/local/php7.3/bin/php bin/console scheduler:execute

5525c8ab1dbe02045a1d0ff7fce06d9b9e33611c
Aurélien <litti.aurelien@gmail.com>
2026-01-10 13:00:45 +0100

    chore: add rector bundle

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true
+    )
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0);
